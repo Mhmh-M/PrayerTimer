773dad44ccaf892377c8ae1df73ce4cd45b069db
MohamedAbdElghany <86925987+Mhmh-M@users.noreply.github.com>
2023-10-09 11:52:01 +0300

    add a new project
    add a new project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/App.jsx",
+    "./src/components/MainContent.jsx",
+    "./src/components/Prayer.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
